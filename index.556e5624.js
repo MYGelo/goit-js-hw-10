9785b75e98073dfbb2dbbfbbf87384d7673e4f57
MYGelo <112314820+MYGelo@users.noreply.github.com>
2023-01-25 22:59:19 +0000

    Deploying to gh-pages from @ MYGelo/goit-js-hw-10@deea83be35dfb73b8bb10e04f47fff45366a171a 🚀

---
+++ index.556e5624.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-box").insertAdjacentHTML("beforebegin","<header><h1>Country Finder</h1></header>");
+//# sourceMappingURL=index.556e5624.js.map
---
+++ index.946b5f2f.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-box").insertAdjacentHTML("beforebegin","<header><h1>Country Finder</h1></header>");
+//# sourceMappingURL=index.946b5f2f.js.map
