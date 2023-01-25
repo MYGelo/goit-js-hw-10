deea83be35dfb73b8bb10e04f47fff45366a171a
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-01-26 01:02:51 +0200

    tesrt 3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector('#search-box');
+searchBox.insertAdjacentHTML(
+  'beforebegin',
+  '<header><h1>Country Finder</h1></header>'
+);
